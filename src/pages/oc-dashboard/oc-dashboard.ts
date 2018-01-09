434ff382eec0821e691b0b6c3a9b7a2a655466bd
codovamobile <cordovamobileapp@gmail.com>
2018-01-09 15:04:32 +0530

    Commit

--- src/pages/oc-dashboard/oc-dashboard.ts
+++ src/pages/oc-dashboard/oc-dashboard.ts
@@ -1,8 +1,19 @@
+
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Nav, /*IonicPage,*/ NavController, NavParams, ToastController, AlertController  } from 'ionic-angular';
+
+import { Dataprovider } from '../../providers/dataprovider';
+import { ApplicationService } from '../../providers/application-service';
+
+import { OcBusinessAddPage } from '../../pages/oc-business-add/oc-business-add';
+import { OcBusinessSinglePage } from '../../pages/oc-business-single/oc-business-single';
+/*
+import { BusinessAddPage } from '../../pages/business-add/business-add';
+import { BusinessListPage } from '../../pages/business-list/business-list';
+*/
 
 /**
- * Generated class for the OcDashboardPage page.
+ * Generated class for the DashboardPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +21,132 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 //@IonicPage()
 @Component({
-  selector: 'page-oc-dashboard',
-  templateUrl: 'oc-dashboard.html',
+	selector: 'page-oc-dashboard',
+	templateUrl: 'oc-dashboard.html',
 })
 export class OcDashboardPage {
+	
+	userdata: any;
+	businesss: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(
+		public navCtrl: NavController,
+		public navParams: NavParams,
+		public dataprovider: Dataprovider,			
+		public toastCtrl: ToastController,
+		public alertCtrl: AlertController ,
+		public nav: Nav,
+		public application_service: ApplicationService
+	) {
+		
+		
+		
+		this.businesss = [];
+		/* 
+		this.business_list = [
+			{
+				"buid": "48",
+				"business_name": "uttam kumar singh",
+				"pan": "DAOPK0909A",
+				"email_address": "",
+				"primary_contact_person": "uttam kumar",
+				"primary_contact_personph": "9015954550",
+				"bu_type": "CA"
+			},
+			{
+				"buid": "55",
+				"business_name": "Vipin Gupta",
+				"pan": "QWEPG9887U",
+				"email_address": "Vipin.kumar@kapspro.com",
+				"primary_contact_person": "Vipin",
+				"primary_contact_personph": "1234567898",
+				"bu_type": "CA"
+			},
+			{
+				"buid": "63",
+				"business_name": "abc5",
+				"pan": "bgapr",
+				"email_address": null,
+				"primary_contact_person": null,
+				"primary_contact_personph": null,
+				"bu_type": "oc"
+			}
+		];
+		*/
+		
+		
+		
+		
+		var self = this;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad OcDashboardPage');
-  }
+		console.log('requestAPI being called...');
+		self.dataprovider.requestAPI(
+			'get',
+			'programming/hbgstapi/api/getbusinessbyid/' + self.application_service.userdata.api_token, 
+			{},
+			'Getting businesses list...',
+			true, /* Token To Not Be Sent To API */
+			
+			function(response) {
+				
+				/* Logging 'Request Has Responded' event */
+				console.log( 'requestAPI responded...' );
+				console.log( 'requestAPI Response: "' + JSON.stringify( response ) + '"' );
+				console.log( 'requestAPI Response Type: ' + response.status );
+				
+				
+				if( response.status != 'failed' ) { // If success
+					
+					self.businesss = response;
+					
+				} else {
+					
+					self.login_form.email = '';
+					self.login_form.password = '';
+					self.login_form.view_password = false;
+					let toast = self.toastCtrl.create({
+						message:	response.message,
+						duration:	10000,
+						cssClass:	"toast-success"
+					});
+					toast.present();
+					
+				}
+				
+				
 
+			}
+		);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad DashboardPage');
+	}
+	
+	open_business_single_page(c) {
+	}
+	
+	open_business_add_page(c) {
+		this.nav.push(OcBusinessAddPage);
+	}
+	
+	/*
+	goto_business_add_page() {
+		this.nav.push( BusinessAddPage );
+	}
+	
+	goto_business_list_page() {
+		this.nav.push( BusinessListPage );
+	}
+	*/
+	
 }
